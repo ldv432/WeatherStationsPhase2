144245452a4066471088629984f9d8ccc5897224
ldv432 <ldv432@gmail.com>
2024-07-07 09:30:01 -0600

    imported components to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import home from './components/home';
+import stationList from './components/stationList'
+import stationDetails from './components/stationDetails'
+import addStation from './components/addStation'
+import about from './components/about'
+
 
 function App() {
   return(
     <div>
-      
+
     </div>
   )
 }
