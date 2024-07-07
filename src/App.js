576d02d7bbb2665fbc36e3c511974b3a939407ee
ldv432 <ldv432@gmail.com>
2024-07-07 09:58:48 -0600

    added basic router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import home from './components/home';
-import stationList from './components/stationList'
-import stationDetails from './components/stationDetails'
-import addStation from './components/addStation'
-import about from './components/about'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/home';
+import StationList from './components/stationList'
+import StationDetails from './components/stationDetails'
+import AddStation from './components/addStation'
+import About from './components/about'
 
 
 function App() {
   return(
-    <div>
-
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><link to="/">Home</link></li>
+            <li><link to="/station-list">List of Stations</link></li>
+            <li><link to="/add-station">Add a station!</link></li>
+            <li><link to="/about">About this site</link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/station-list" element={<StationList />}></Route>
+          <Route path="/add-station" element={<AddStation />}></Route>
+          <Route path="/station/:id" element={<StationDetails />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
