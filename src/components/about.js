cd39ae40f59ade26e1ee87345380cec743a96187
ldv432 <ldv432@gmail.com>
2024-07-07 10:02:44 -0600

    built out about component

---
+++ src/components/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>Our webpage lists different weather stations! You can also add a weather station if you don't see the one you're looking for!</h1>
+    </div>
+  );
+}
+
+export default About;
